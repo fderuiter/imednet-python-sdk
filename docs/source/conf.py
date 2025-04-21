44a60be66386bafd4a876d1ca26b36093c5487b3
Frederick de Ruiter <fderuiter@brightresearchpartners.com>
2025-04-21 17:09:40 -0500

    docs: update Sphinx configuration for improved documentation generation and type hinting support

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,24 +3,30 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../')) # Add project root to path
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "iMednet Python SDK"
+project = "imednet-python-sdk" # Match pyproject.toml
 copyright = "2025, Frederick de Ruiter"
 author = "Frederick de Ruiter"
-release = "0.1.0"
+release = "0.1.0" # Match pyproject.toml
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.napoleon",  # Added Napoleon
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.githubpages",
-    "sphinx_rtd_theme",  # Added theme extension
+    "sphinx.ext.autodoc",       # Core library for html generation from docstrings
+    "sphinx.ext.napoleon",      # Support for Google and NumPy style docstrings
+    "sphinx.ext.viewcode",      # Add links to highlighted source code
+    "sphinx.ext.intersphinx",   # Link to other projects' documentation
+    "sphinx.ext.githubpages",   # Support for GitHub Pages
+    "sphinx_rtd_theme",         # Read the Docs theme
+    "sphinx_autodoc_typehints", # Automatically document typehints
+    "sphinx_copybutton",        # Add a 'copy to clipboard' button to code blocks
 ]
 
 templates_path = ["_templates"]
@@ -29,10 +35,12 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"  # Set the theme
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
-# Napoleon settings
+# -- Extension configuration -------------------------------------------------
+
+# Napoleon settings (Google style)
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False  # We are using Google style
 napoleon_include_init_with_doc = True
@@ -51,9 +59,25 @@ napoleon_attr_annotations = True
 # Intersphinx mapping
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
-    "requests": ("https://requests.readthedocs.io/en/latest/", None),
+    "httpx": ("https://www.python-httpx.org/", None), # Changed from requests
     "pydantic": ("https://docs.pydantic.dev/latest/", None),
 }
 
 # Autodoc settings
 autodoc_member_order = "bysource"
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__'
+}
+
+# Typehints settings
+set_type_checking_flag = True # Enable type checking for typehints
+typehints_fully_qualified = False # Don't show full path for types
+always_document_param_types = True # Ensure param types are documented
+
+# Copybutton settings
+copybutton_prompt_text = r">>> |\.\.\. |\$ " # Regex to exclude prompts
+copybutton_prompt_is_regexp = True
