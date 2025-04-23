2f341f3dd02eecbebe66fa48c692fa08bff0e453
Frederick de Ruiter <fderuiter@brightresearchpartners.com>
2025-04-23 13:31:32 -0500

    refactor: update endpoint and model __init__ files to improve documentation and exports

--- imednet/models/__init__.py
+++ imednet/models/__init__.py
@@ -1,3 +1,37 @@
-"""Top-level imednet package."""
+"""Models package for the iMedNet SDK.
 
-__all__: list[str] = ["ImednetSDK"]
+This package contains all data models used by the SDK to represent iMedNet resources.
+"""
+
+from imednet.models.codings import Coding
+from imednet.models.forms import Form
+from imednet.models.intervals import FormSummary, Interval
+from imednet.models.jobs import Job
+from imednet.models.queries import Query, QueryComment
+from imednet.models.record_revisions import RecordRevision
+from imednet.models.records import Keyword, Record
+from imednet.models.sites import Site
+from imednet.models.subjects import Subject, SubjectKeyword
+from imednet.models.users import Role, User
+from imednet.models.variables import Variable
+from imednet.models.visits import Visit
+
+__all__: list[str] = [
+    "Coding",
+    "Form",
+    "FormSummary",
+    "Interval",
+    "Job",
+    "Keyword",
+    "Query",
+    "QueryComment",
+    "Record",
+    "RecordRevision",
+    "Role",
+    "Site",
+    "Subject",
+    "SubjectKeyword",
+    "User",
+    "Variable",
+    "Visit",
+]
