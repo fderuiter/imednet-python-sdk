cdc4e39058f68080baa4395ca14603b505128cd0
Frederick de Ruiter <127706008+fderuiter@users.noreply.github.com>
2025-06-06 18:29:39 -0500

    Clean up workflows __init__

--- imednet/workflows/__init__.py
+++ imednet/workflows/__init__.py
@@ -1,23 +1,5 @@
-"""Top-level imednet package."""
+"""Top-level workflows package."""
 
-# --- Original (now commented out) ---
-# from .query_management import close_queries, create_query, get_query_details
-# from .record_mapper import map_records_to_model
-# from .record_update import update_record_data
-# from .register_subjects import register_subjects
-# from .subject_data import get_subject_data
-
-# --- Correct workflow class/function imports ---
-    "VisitCompletionWorkflow",
-from .query_management import QueryManagementWorkflow
-from .record_mapper import RecordMapper
-from .record_update import RecordUpdateWorkflow
-from .register_subjects import RegisterSubjectsWorkflow
-from .site_performance import SitePerformanceWorkflow
-from .study_structure import get_study_structure
-from .subject_data import SubjectDataWorkflow
-from .veeva_push import VeevaPushWorkflow
-from .subject_enrollment_dashboard import SubjectEnrollmentDashboard
 from .visit_completion import VisitCompletionWorkflow
 
 __all__ = [
