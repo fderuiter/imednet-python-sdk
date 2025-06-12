42d0b918e10eb9dc5db308383a5b082f3a2e5c1d
Frederick de Ruiter <127706008+fderuiter@users.noreply.github.com>
2025-06-12 13:45:39 -0500

    Add pandas utilities for records (#135)

--- imednet/utils/__init__.py
+++ imednet/utils/__init__.py
@@ -6,10 +6,27 @@ from .dates import format_iso_datetime, parse_iso_datetime
 from .filters import build_filter_string
 from .typing import DataFrame, JsonDict
 
+
+def __getattr__(name: str):
+    if name in {"records_to_dataframe", "export_records_csv"}:
+        from .pandas import export_records_csv, records_to_dataframe
+
+        globals().update(
+            {
+                "records_to_dataframe": records_to_dataframe,
+                "export_records_csv": export_records_csv,
+            }
+        )
+        return globals()[name]
+    raise AttributeError(name)
+
+
 __all__ = [
     "parse_iso_datetime",
     "format_iso_datetime",
     "build_filter_string",
+    "records_to_dataframe",
+    "export_records_csv",
     "JsonDict",
     "DataFrame",
 ]
