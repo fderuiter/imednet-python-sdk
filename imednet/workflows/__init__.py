b7a82d2bde0a39177719650112f72c01103d9063
Frederick de Ruiter <127706008+fderuiter@users.noreply.github.com>
2025-06-12 16:55:00 -0500

    refactor(workflows): clean init (#187)

--- imednet/workflows/__init__.py
+++ imednet/workflows/__init__.py
@@ -1,13 +1,5 @@
-"""Top-level imednet package."""
+"""Workflow helpers built on top of the iMednet SDK."""
 
-# --- Original (now commented out) ---
-# from .query_management import close_queries, create_query, get_query_details
-# from .record_mapper import map_records_to_model
-# from .record_update import update_record_data
-# from .register_subjects import register_subjects
-# from .subject_data import get_subject_data
-
-# --- Correct workflow class/function imports ---
 from .query_management import QueryManagementWorkflow
 from .record_mapper import RecordMapper
 from .record_update import RecordUpdateWorkflow
@@ -16,16 +8,6 @@ from .study_structure import async_get_study_structure, get_study_structure
 from .subject_data import SubjectDataWorkflow
 
 __all__ = [
-    # Original (commented out):
-    # "close_queries",
-    # "create_query",
-    # "get_query_details",
-    # "map_records_to_model",
-    # "update_record_data",
-    # "register_subjects",
-    # "get_subject_data",
-    #
-    # Updated:
     "QueryManagementWorkflow",
     "RecordMapper",
     "RecordUpdateWorkflow",
